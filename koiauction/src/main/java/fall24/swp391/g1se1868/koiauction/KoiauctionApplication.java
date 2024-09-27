96ba842e1bd27651a9ebd84b9667546e982e342e
haitauhai <haitthcs@gmail.com>
2024-09-27 22:13:11 +0700

    Setup environment

---
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/KoiauctionApplication.java
@@ -0,0 +1,13 @@
+package fall24.swp391.g1se1868.koiauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiauctionApplication.class, args);
+	}
+
+}
---
+++ koiauction/src/test/java/fall24/swp391/g1se1868/koiauction/KoiauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package fall24.swp391.g1se1868.koiauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
