84126e7302ef66cae420bac43dff059d0f5af284
nguyendaobach <nguyendaobach@gmail.com>
2024-09-29 18:35:34 +0700

    jwt and login update

---
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/controller/SecurityController.java
@@ -0,0 +1,24 @@
+package fall24.swp391.g1se1868.koiauction.controller;
+
+import fall24.swp391.g1se1868.koiauction.model.User;
+import fall24.swp391.g1se1868.koiauction.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String Login (@RequestBody User user){
+        return userService.login(user);
+    }
+}
--- koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/service/UserService.java
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/service/UserService.java
@@ -3,6 +3,11 @@ package fall24.swp391.g1se1868.koiauction.service;
 import fall24.swp391.g1se1868.koiauction.model.User;
 import fall24.swp391.g1se1868.koiauction.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -15,6 +20,31 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    AuthenticationManager authManager;
+
+    @Autowired
+    JwtService jwtService;
+
+    private BCryptPasswordEncoder encoder =new BCryptPasswordEncoder(12);
+
+    public User register(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setRole("User");
+        return userRepository.save(user);
+    }
+
+    public String login(User user) {
+        Authentication authentication
+                =authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+        if (authentication.isAuthenticated()){
+            return jwtService.generateToken(user.getUserName());
+        }
+        else {
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+    }
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -56,9 +86,7 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    public static void main(String[] args) {
-        UserService u = new UserService();
-        System.out.println(u.getAllUsers().toString());
-    }
+
+
 }
 
