682b1afffca178d858b19ddf9768bb14d6c076a5
nguyendaobach <nguyendaobach@gmail.com>
2024-10-10 22:35:11 +0700

    update koi detail

--- koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/repository/KoiMediaRepository.java
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/repository/KoiMediaRepository.java
@@ -1,9 +1,15 @@
 package fall24.swp391.g1se1868.koiauction.repository;
 
+import fall24.swp391.g1se1868.koiauction.model.KoiFish;
 import fall24.swp391.g1se1868.koiauction.model.KoiMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface KoiMediaRepository extends JpaRepository<KoiMedia,Integer> {
+    Optional<KoiMedia> findByKoiIDAndMediaType(KoiFish koiID, String mediaType);
+    List<KoiMedia> findByKoiID(KoiFish koiID);
 }
