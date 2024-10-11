f34e8588d8a84cfd390cbc4592a11b38fc8bd94e
haitauhai <haitthcs@gmail.com>
2024-10-12 01:11:28 +0700

    Config WebSocket

--- koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/repository/BidRepository.java
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/repository/BidRepository.java
@@ -2,6 +2,11 @@ package fall24.swp391.g1se1868.koiauction.repository;
 
 import fall24.swp391.g1se1868.koiauction.model.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BidRepository extends JpaRepository<Bid, Long> {
+    List<Bid> findByAuctionId(Integer auctionId);
 }
