a801bdbf58064bf04852b2e40fd9d8131ea2ebf4
haitauhai <haitthcs@gmail.com>
2024-11-02 00:23:57 +0700

    update Chat API

--- koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/service/ChatService.java
+++ koiauction/src/main/java/fall24/swp391/g1se1868/koiauction/service/ChatService.java
@@ -23,10 +23,10 @@ public class ChatService {
         return chatMessageRepository.save(message);
     }
 
-    public List<Chat> getChatMessages(Integer senderId, Integer receiverId, int page) {
+    public Page<Chat> getChatMessages(Integer senderId, Integer receiverId, int page) {
         Pageable pageable = PageRequest.of(page, 20);
         Page<Chat> chatPage = chatMessageRepository.findMessages(senderId, receiverId, pageable);
-        return chatPage.getContent();
+        return chatPage;
     }
 
     public List<UserChat> getUserChats(Integer userId) {
